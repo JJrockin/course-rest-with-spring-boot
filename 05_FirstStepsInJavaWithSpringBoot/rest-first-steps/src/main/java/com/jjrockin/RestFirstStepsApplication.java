72cf6b7e740e38d9b0ea3d399554a453a7f863da
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-06-15 21:48:40 -0300

    First steps

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-first-steps/src/main/java/com/jjrockin/RestFirstStepsApplication.java
@@ -0,0 +1,13 @@
+package com.jjrockin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFirstStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFirstStepsApplication.class, args);
+	}
+
+}
