fd9488176347d798a8c7b05a0f3caa7af92030f7
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-06-19 11:39:40 -0300

    Module 7 - Working with HTTP verbs

---
+++ 07_WorkingWithVerbs/working-with-verbs/src/main/java/com/jjrockin/WorkingWithVerbsApplication.java
@@ -0,0 +1,13 @@
+package com.jjrockin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithVerbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithVerbsApplication.class, args);
+	}
+
+}
