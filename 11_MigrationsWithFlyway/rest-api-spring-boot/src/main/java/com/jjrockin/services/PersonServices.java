69d2f1304a385bac9387243556f85bd79d5d1934
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-06-21 11:44:24 -0300

    Using migrations with Flyway

---
+++ 11_MigrationsWithFlyway/rest-api-spring-boot/src/main/java/com/jjrockin/services/PersonServices.java
@@ -0,0 +1,60 @@
+package com.jjrockin.services;
+
+import com.jjrockin.data.vo.v1.PersonVO;
+import com.jjrockin.exceptions.ResourceNotFoundException;
+import com.jjrockin.mapper.ModelMapper;
+import com.jjrockin.model.Person;
+import com.jjrockin.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+@Service
+public class PersonServices {
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    @Autowired
+    PersonRepository repository;
+
+    public List<PersonVO> findAll(){
+
+        logger.info("Finding all people");
+
+        return ModelMapper.parseListObjects(repository.findAll(), PersonVO.class);
+    }
+    public PersonVO findById(Long id){
+
+        logger.info("Finding one person");
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
+        return ModelMapper.parseObject(entity, PersonVO.class);
+    }
+
+    public PersonVO create(PersonVO person) {
+        logger.info("Creating one person");
+        var entity = ModelMapper.parseObject(person, Person.class);
+        var vo = ModelMapper.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
+    }
+    public PersonVO update(PersonVO person) {
+        logger.info("Updating one person");
+
+        var entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+        var vo = ModelMapper.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
+    }
+
+    public void delete(Long id){
+        logger.info("Deleting one person");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID."));
+        repository.delete(entity);
+    }
+}
---
+++ 11_MigrationsWithFlyway/rest-api-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1,'São Paulo - Brasil','Ayrton','Male','Senna'),
+(2,'Dublin - Ireland','Maria','Female','Carolina'),
+(3,'Recife - Brasil','José','Male','Jeronimo'),
+(4,'Salvador - Brasil','Bruno','Male','Silva'),
+(5,'Feira de Santana - Brasil','Telma','Female','Moraes'),
+(6,'Smiljan - Croatia','Nikola','Male','Tesla');
